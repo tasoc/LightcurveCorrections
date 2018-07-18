2b4c422655f4a0193a7296de138d17181362442b
Lindsey Carboneau <lmcarboneau@gmail.com>
2018-07-18 14:26:39 +0200

    Major cleaning and logic restructuring
    Pay special attention to TODO's

--- BaseCorrection.py
+++ BaseCorrection.py
@@ -65,94 +65,13 @@ class BaseCorrection(object):
         self.input_folder = input_folder
         #TODO: others
 
-        logger.info('STARID = %d', self.starid) #TODO: others
-
-        self._status = STATUS.UNKNOWN
-        self._details = {}
-        #TODO: others
-
-        # Directory(s) to save output files:
-        #TODO: logic for directing output? CBV vs LC
-        self.output_folder = os.path.join(
-            output_folder,
-            # self.datasource, #TODO: if that is included/relevant 
-            '{0:011d}'.format(self.starid)[0:5]
-        )
-
-        #TODO: Set directory for diagnostics? photometry `plot` option
-
-        # Init table that will be filled with lightcurve stuff:
-        self.lightcurve = Table()
-
-        #TODO: This is where photometry differentiates between input datasources (ffi vs tpf); should prob be where we filter on fits vs txt
-        # e.g., if self.datasource == 'fits': ------; if self.datasource == 'txt': ------;
-        # consider using 'pho' or similar instead of 'fits' --> indicates output from photometry
-
-        #TODO: whitespace/scope verification after filter implementation
-        if camera is None or ccd is None:
-            #TODO: should probably exist inside of a datasource filter
-            raise ValueError("CAMERA and CCD keywords must be provided for %s targets", self.datasource)
-
-        self.camera = camera # TESS camera
-        self.ccd = ccd # TESS CCD
-        # TODO: redirect to proper path - might be "area" and not "camera_ccd" based? Talk to Rasmus!!
-        self.catalog_file = os.path.join(input_folder, 'catalog_camera{0:d}_ccd{1:d}.sqlite'.format(self.camera, self.ccd))
-
-        logger.debug('CAMERA = %s', self.camera)
-        logger.debug('CCD = %s', self.ccd)
-        logger.debug('Catalog file: %s', self.catalog_file)
-
-        # Load info on target
-        conn = sqlite3.connect(self.catalog_file)
-        conn.row_factory = sqlite3.Row
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM catalog WHERE starid={0:d};".format(self.starid))
-        target = cursor.fetchone()
-
-        if target is None:
-            raise IOError("Target could not be found in catalog: {0:d}".format(self.starid))
-        self.target_tmag = target['tmag'] # TESS Magnitude of target
-        self.target_pos_ra = target['ra']
-        self.target_pos_dec = target['decl'] # TODO: check DB - should be just 'dec'?
-        # NOTE: neither of these are J2000 - do we need? is it in the corrections todo?
-
-        # TODO: other database info loading
-        cursor.close()
-        conn.close()
-
 
     @property
     def status(self):
         """The status of the corrections. From :py:class:`STATUS`."""
         return self._status
 
-    @property
-    def catalog(self):
-        """
-        Catalog of targets on current CCD area
-
-        The table contains the following columns:
-        * ``starid``:
-        * ``tmag``:
-        * ``ra``:
-        * ``dec``:
-        * ``ccdarea``:
-        * ``variability``:
-        * TODO: other info for corrections?
-
-        Returns:
-            ``astropy.table.Table``: Table with all the targets on the current CCD area
-
-        Example:
-            if ``corr`` is an instance of :py:class:`BaseCorrection`:
-                >>> corr.catalog['tmag']
-                >>> corr.catalog[('starid', 'tmag', 'row', 'column')]
-        """
-        
-        if not self._catalog:
-            dbconn = sqlite3.connect(self.catalog_file)
-
-    def do_corrections(self):
+    def do_correction(self):
         # Following photometry structure 
         """
         Apply corrections to Lightcurve.
@@ -180,8 +99,8 @@ class BaseCorrection(object):
             :py:func:`do_corrections`
         """
 
-        # run the corrections
-        self._status = self.do_corrections(*args, **kwargs)
+        # run the correction
+        self._status = self.do_correction(*args, **kwargs)
 
         #check that the status has been changed
         if self._status == STATUS.UNKNOWN:
--- coarse_correction.py
+++ coarse_correction.py
@@ -21,14 +21,6 @@ class CoarseCorrection(BaseCorrection):
 
         logger = logging.getLogger(__name__)
 
-        # Generate list of stars to fit:
-        cat = self.catalog
-        # TODO: look into this. I think it makes sense to make a list of everyone nearby
-        #       on area in order to do other ensemble work in detrending (instead of cotrending CBVs)
-
-        # TODO: make sure the target is in the catalog itself! if it's being used that way?
-        #       otherwise it should be in self.etc but might be relevant later??
-
         ###
         # NOTE: Placeholder for coarse classification logic
         #
