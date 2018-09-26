14606fd7c7e1e9bfba97bba01b3fe8bef458b77a
Lindsey Carboneau <lmcarboneau@gmail.com>
2018-09-26 16:44:38 -0400

    Adding dummy module

---
+++ corrections/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from .BaseCorrection import BaseCorrection
+#from .taskmanager import TaskManager 
+# NOTE: TaskManager is technically implemented already
+#       but isn't being properly utilized yet
+# TODO: Fix above
