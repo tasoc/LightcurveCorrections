504cbfedbc1a7e8a0a26d072028b6a2d2ad03463
Mikkel Nørup Lund <mikkelnl@phys.au.dk>
2018-11-23 12:09:36 +0100

    distance calc added to median filter, code clean-up, calc of interpolation for uncertainties

--- cbv_correction/cbv_weights.py
+++ cbv_correction/cbv_weights.py
@@ -46,11 +46,15 @@ def reduce_mode(x):
 	x = kde.support
 	return x[np.argmax(pdf)]
 
-def ndim_med_filt(d, v, x, n):
+def ndim_med_filt(v, x, n, dist='euclidean', mad_frac=2):
+	
+	d = distance.cdist(x, x, dist)
+	
+	
 	idx = np.zeros_like(v, dtype=bool)
 	for i in range(v.shape[0]):
 		idx_sort = np.argsort(d[i,:])
-		xx = x[idx_sort, :][1:n+1, :]
+#		xx = x[idx_sort, :][1:n+1, :]
 		vv= v[idx_sort][1:n+1] # sort values according to distance from point
 		
 		vm = np.median(vv) # median value of n nearest points
@@ -74,10 +78,20 @@ def ndim_med_filt(d, v, x, n):
 #			plt.show()
 #			sys.exit()
 			
-		if (v[i]<vm+2*mad) & (v[i]>vm-2*mad):
+		if (v[i]<vm+mad_frac*mad) & (v[i]>vm-mad_frac*mad):
 			idx[i] = True
 	return idx			
 #------------------------------------------------------------------------------
+	
+# TODO: include removal of simple trend in nd median filtering
+# TODO: Do nd median filtering before binning
+# TODO: Try binned_stats over hexbin (scipy.stats.binned_statistic_dd)
+# TODO: Inclusion of tmag dimension - normalise "distance" in 3D space
+# TODO: Better/Other CBV areas
+# TODO: Tests with polar coordinates in combination with other CBV areas	
+# TODO: mode vs. median in binning
+
+
 if __name__ == '__main__':
 	# Pick a sector, any sector....
 	sector = 0
@@ -95,10 +109,9 @@ if __name__ == '__main__':
 	print(cbv_areas)
 	
 	colormap = plt.cm.PuOr #or any other colormap
-	normalize1 = colors.Normalize(vmin=0, vmax=0.5)
-	normalize2 = colors.Normalize(vmin=0, vmax=0.1)
+	normalize1 = colors.Normalize(vmin=0, vmax=0.45)
+	normalize2 = colors.Normalize(vmin=0, vmax=0.15)
 	normalize3 = colors.Normalize(vmin=0, vmax=0.05)
-#	plt.scatter(x,y,z,s=5, cmap=colormap, norm=normalize, marker='*')
 
 	fig = plt.figure(figsize=(15,6))
 	ax1 = fig.add_subplot(231)
@@ -123,7 +136,6 @@ if __name__ == '__main__':
 	
 #	minlat = 100
 #	maxlat = 0
-
 
 	# Loop through the CBV areas:
 	# - or run them in parallel - whatever you like!
@@ -169,10 +181,6 @@ if __name__ == '__main__':
 #			if pos_mag[jj, 1]>maxlat:
 #				maxlat = pos_mag[jj, 1]	
 				
-				
-			
-
-
 		results=np.column_stack((results, pos_mag))
 		print(results.shape, np.min(results[:,1]), np.max(results[:,1]), np.median(np.abs(results[:,1])), np.median(np.abs(results[:,2])), np.median(np.abs(results[:,3])))
 		
@@ -182,102 +190,95 @@ if __name__ == '__main__':
 		VALS2 = np.abs(results[:,2])
 		VALS3 = np.abs(results[:,3])
 		
+		# Perform binning
+		
+		# CBV values
 		hb1 = ax1.hexbin(ELON, ELAT, C=VALS1, gridsize=10, reduce_C_function=reduce_mode, cmap=colormap, norm=normalize1, marginals=False)
 		hb2 = ax2.hexbin(ELON, ELAT, C=VALS2, gridsize=10, reduce_C_function=reduce_mode, cmap=colormap, norm=normalize2, marginals=False)
 		hb3 = ax3.hexbin(ELON, ELAT, C=VALS3, gridsize=10, reduce_C_function=reduce_mode, cmap=colormap, norm=normalize3, marginals=False)
+		
+		# CBV values scatter
+		hb4 = ax4.hexbin(ELON, ELAT, C=VALS1, gridsize=10, reduce_C_function=reduce_std, cmap=colormap, norm=normalize1, marginals=False)
+		hb5 = ax5.hexbin(ELON, ELAT, C=VALS2, gridsize=10, reduce_C_function=reduce_std, cmap=colormap, norm=normalize2, marginals=False)
+		hb6 = ax6.hexbin(ELON, ELAT, C=VALS3, gridsize=10, reduce_C_function=reduce_std, cmap=colormap, norm=normalize3, marginals=False)
+
+
 		
 #		scipy.stats.binned_statistic_dd
 		
 		
 		# Get values and vertices of hexbinning
 		zvals1 = hb1.get_array();		verts1 = hb1.get_offsets()
-		dist1 = distance.cdist(verts1, verts1, 'euclidean')
-		idx1 = ndim_med_filt(dist1, zvals1, verts1, 6)
-		ax1.plot(verts1[~idx1,0], verts1[~idx1,1], marker='.', ms=1, ls='', color='r')
-		zvals1, verts1 = zvals1[idx1], verts1[idx1] 
-		
-		
-#		for kk in range(3):
-#			mad1 = MAD_model(zvals1-np.nanmedian(zvals1))
-#			idx = (zvals1<np.nanmedian(zvals1)+2.5*mad1) & (zvals1>np.nanmedian(zvals1)-2.5*mad1)
-#			zvals1, verts1 = zvals1[idx], verts1[idx] 
-#		
-#		
 		zvals2 = hb2.get_array();		verts2 = hb2.get_offsets()
-		dist2 = distance.cdist(verts2, verts2, 'euclidean')
-		# TODO: include removal of simple trend
-		idx2 = ndim_med_filt(dist2, zvals2, verts2, 6)
+		zvals3 = hb3.get_array();		verts3 = hb3.get_offsets()	
+		zvals4 = hb4.get_array();		verts4 = hb4.get_offsets()	
+		zvals5 = hb5.get_array();		verts5 = hb5.get_offsets()	
+		zvals6 = hb6.get_array();		verts6 = hb6.get_offsets()	
+		
+		# Bins to keed for interpolation
+		idx1 = ndim_med_filt(zvals1, verts1, 6)
+		idx2 = ndim_med_filt(zvals2, verts2, 6)
+		idx3 = ndim_med_filt(zvals3, verts3, 6)
+		idx4 = ndim_med_filt(zvals4, verts4, 6)
+		idx5 = ndim_med_filt(zvals5, verts5, 6)
+		idx6 = ndim_med_filt(zvals6, verts6, 6)
+		
+		# Plot removed bins
+		ax1.plot(verts1[~idx1,0], verts1[~idx1,1], marker='.', ms=1, ls='', color='r')
 		ax2.plot(verts2[~idx2,0], verts2[~idx2,1], marker='.', ms=1, ls='', color='r')
-		zvals2, verts2 = zvals2[idx2], verts2[idx2] 
-		
-#		for kk in range(3):
-#			mad2 = MAD_model(zvals2-np.nanmedian(zvals2))
-#			idx2 = (zvals2<np.nanmedian(zvals2)+2.5*mad2) & (zvals2>np.nanmedian(zvals2)-2.5*mad2)
-#			zvals2, verts2 = zvals2[idx2], verts2[idx2] 
-#		
-#		
-		zvals3 = hb3.get_array();		verts3 = hb3.get_offsets()
-		dist3 = distance.cdist(verts3, verts3, 'euclidean')
-		# TODO: include removal of simple trend
-		idx3 = ndim_med_filt(dist3, zvals3, verts3, 6)
 		ax3.plot(verts3[~idx3,0], verts3[~idx3,1], marker='.', ms=1, ls='', color='r')
-		zvals3, verts3 = zvals3[idx3], verts3[idx3] 
+		ax4.plot(verts4[~idx4,0], verts4[~idx4,1], marker='.', ms=1, ls='', color='r')
+		ax5.plot(verts5[~idx5,0], verts5[~idx5,1], marker='.', ms=1, ls='', color='r')
+		ax6.plot(verts6[~idx6,0], verts6[~idx6,1], marker='.', ms=1, ls='', color='r')
 		
-#		for kk in range(3):
-#			mad3 = MAD_model(zvals3-np.nanmedian(zvals3))
-#			idx3 = (zvals3<np.nanmedian(zvals3)+2.5*mad3) & (zvals3>np.nanmedian(zvals3)-2.5*mad3)
-#			zvals3, verts3 = zvals3[idx3], verts3[idx3] 
-#		
+		# Trim binned values before interpolation
+		zvals1, verts1 = zvals1[idx1], verts1[idx1] 
+		zvals2, verts2 = zvals2[idx2], verts2[idx2] 
+		zvals3, verts3 = zvals3[idx3], verts3[idx3] 
+		zvals4, verts4 = zvals4[idx4], verts4[idx4] 
+		zvals5, verts5 = zvals5[idx5], verts5[idx5] 
+		zvals6, verts6 = zvals6[idx6], verts6[idx6] 
+		
+
 #		# Interpolate
 #		rbfi1 = Rbf(verts1[:,0], verts1[:,1], zvals1, smooth=1, function='linear')
 #		rbfi2 = Rbf(verts2[:,0], verts2[:,1], zvals2, smooth=1, function='linear')
 #		rbfi3 = Rbf(verts3[:,0], verts3[:,1], zvals3, smooth=1, function='linear')
-#		
-#		diff = zvals1 - rbfi1(verts1[:,0], verts1[:,1])
-#		mad1_2 = MAD_model(diff)
-#		idx = (diff<3*mad1_2) & (diff>-3*mad1_2)
-#		zvals1, verts1 = zvals1[idx], verts1[idx] 
-		
+
 		rbfi1 = Rbf(verts1[:,0], verts1[:,1], zvals1, smooth=1)
 		rbfi2 = Rbf(verts2[:,0], verts2[:,1], zvals2, smooth=1)
 		rbfi3 = Rbf(verts3[:,0], verts3[:,1], zvals3, smooth=1)
+		rbfi4 = Rbf(verts4[:,0], verts4[:,1], zvals4, smooth=1)
+		rbfi5 = Rbf(verts5[:,0], verts5[:,1], zvals5, smooth=1)
+		rbfi6 = Rbf(verts6[:,0], verts6[:,1], zvals6, smooth=1)
 			
-#		plt.figure()
-#		plt.plot(diff)
-		
-		
-		
 #		rbfi1 = SmoothBivariateSpline(verts1[:,0], verts1[:,1], zvals1)
 #		rbfi2 = SmoothBivariateSpline(verts2[:,0], verts2[:,1], zvals2)
 #		rbfi3 = SmoothBivariateSpline(verts3[:,0], verts3[:,1], zvals3)
 		
+		
+		# Plot resulting interpolation
 		x1 = np.linspace(verts1[:,0].min(), verts1[:,0].max(), 100); y1 = np.linspace(verts1[:,1].min(), verts1[:,1].max(), 100); xv1, yv1 = np.meshgrid(x1, y1)
 		x2 = np.linspace(verts2[:,0].min(), verts2[:,0].max(), 100); y2 = np.linspace(verts2[:,1].min(), verts2[:,1].max(), 100); xv2, yv2 = np.meshgrid(x2, y2)
 		x3 = np.linspace(verts3[:,0].min(), verts3[:,0].max(), 100); y3 = np.linspace(verts3[:,1].min(), verts3[:,1].max(), 100); xv3, yv3 = np.meshgrid(x3, y3)
+		x4 = np.linspace(verts4[:,0].min(), verts4[:,0].max(), 100); y4 = np.linspace(verts4[:,1].min(), verts4[:,1].max(), 100); xv4, yv4 = np.meshgrid(x4, y4)
+		x5 = np.linspace(verts5[:,0].min(), verts5[:,0].max(), 100); y5 = np.linspace(verts5[:,1].min(), verts5[:,1].max(), 100); xv5, yv5 = np.meshgrid(x5, y5)
+		x6 = np.linspace(verts6[:,0].min(), verts6[:,0].max(), 100); y6 = np.linspace(verts6[:,1].min(), verts6[:,1].max(), 100); xv6, yv6 = np.meshgrid(x6, y6)
 		
 #		xv1, yv1 = np.meshgrid(np.sort(list(set(verts1[:,0]))), np.sort(list(set(verts1[:,1]))))
 #		from scipy.interpolate import griddata
 #		grid_z0 = griddata(verts1, zvals1, (xv1, yv1), method='nearest')
-#		print(grid_z0)
-		
+
 		ax1_2.contourf(xv1, yv1, rbfi1(xv1, yv1), cmap=colormap, norm=normalize1)
-#		ax1_2.contourf(xv1, yv1, grid_z0, cmap=colormap, norm=normalize1)
-		ax2_2.contourf(xv2, yv2, rbfi2(xv2, yv2), cmap=colormap, norm=normalize1)
-		ax3_2.contourf(xv3, yv3, rbfi3(xv3, yv3), cmap=colormap, norm=normalize1)
-		
-#		ax1_2.contourf(xv1, yv1, rbfi1.ev(xv1, yv1), cmap=colormap, norm=normalize1)
-#		ax2_2.contourf(xv2, yv2, rbfi2.ev(xv2, yv2), cmap=colormap, norm=normalize1)
-#		ax3_2.contourf(xv3, yv3, rbfi3.ev(xv3, yv3), cmap=colormap, norm=normalize1)
-		
-		
-#		median_filter(rbfi1(xv1, yv1))
-#		
+		ax2_2.contourf(xv2, yv2, rbfi2(xv2, yv2), cmap=colormap, norm=normalize2)
+		ax3_2.contourf(xv3, yv3, rbfi3(xv3, yv3), cmap=colormap, norm=normalize3)
+		ax4_2.contourf(xv4, yv4, rbfi4(xv4, yv4), cmap=colormap, norm=normalize1)
+		ax5_2.contourf(xv5, yv5, rbfi5(xv5, yv5), cmap=colormap, norm=normalize2)
+		ax6_2.contourf(xv6, yv6, rbfi6(xv6, yv6), cmap=colormap, norm=normalize3)
+
 #		ax2.hexbin(results[:,-2], results[:,-1], C=np.abs(results[:,2]), gridsize=10, reduce_C_function=np.median, cmap=colormap, norm=normalize2, marginals=False)
 #		ax3.hexbin(results[:,-2], results[:,-1], C=np.abs(results[:,3]), gridsize=10, reduce_C_function=np.median, cmap=colormap, norm=normalize3, marginals=False)
 		
-		ax4.hexbin(ELON, ELAT, C=VALS1, gridsize=10, reduce_C_function=reduce_std, cmap=colormap, norm=normalize1, marginals=False)
-		ax5.hexbin(ELON, ELAT, C=VALS2, gridsize=10, reduce_C_function=reduce_std, cmap=colormap, norm=normalize2, marginals=False)
-		ax6.hexbin(ELON, ELAT, C=VALS3, gridsize=10, reduce_C_function=reduce_std, cmap=colormap, norm=normalize3, marginals=False)
 
 
 #	print(minlat, maxlat)
